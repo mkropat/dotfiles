3931e5c1a52dfba7d234ae8b7ff2257ee017a7da
Michael Kropat <mail@michael.kropat.name>
2018-09-02 16:51:29 -0400

    Create user.js

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("dom.battery.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("signon.rememberSignons", false);
