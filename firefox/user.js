d70a7d64974e5ea4e07938b366c986a4c25f9a3c
Michael Kropat <mail@michael.kropat.name>
2019-04-09 22:49:31 -0400

    Update user.js

--- firefox/user.js
+++ firefox/user.js
@@ -1,6 +1,8 @@
-user_pref("dom.webnotifications.enabled", false); // disable notifications prompt
-user_pref("signon.rememberSignons", false);
+user_pref("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream"); // no download prompt for unknown mime types
 user_pref("browser.startup.page", 3); // restore previous session
+user_pref("dom.webnotifications.enabled", false); // disable notifications prompt
+user_pref("general.warnOnAboutConfig", false);
+user_pref("signon.rememberSignons", false);
 
 // Privacy tweaks
 user_pref("dom.battery.enabled", false);
