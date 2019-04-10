42860633356c24fe10bdba1f19cff5585a1d96cc
Michael Kropat <mail@michael.kropat.name>
2019-04-09 20:44:52 -0400

    Update user.js

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,11 @@
-user_pref("dom.battery.enabled", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("media.peerconnection.enabled", false);
+user_pref("dom.webnotifications.enabled", false); // disable notifications prompt
 user_pref("signon.rememberSignons", false);
+user_pref("browser.startup.page", 3); // restore previous session
+
+// Privacy tweaks
+user_pref("dom.battery.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+
+// disable Pocket
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("extensions.pocket.enabled", false);
