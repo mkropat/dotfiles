b3723cb9b05250bd1c4acde9f338eafc41c2614a
Michael Kropat <michael.can.code@kropat.email>
2022-12-10 14:08:11 -0500

    Install repos automatically

--- install.sh
+++ install.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-install() {
+set -e
+
+install_link() {
   mkdir -p "$HOME/$(dirname "$1")"
   ln -fs "$PWD/$1" "$HOME/$1"
 }
 
-install .config/nvim/init.vim
-install .inputrc
-install .profile
-install .vim/vimrc
+install_link .config/nvim/init.vim
+install_link .inputrc
+install_link .profile
+install_link .vim/vimrc
 
 for d in ~/.mozilla/firefox/*.*/; do
   ln -fs "$PWD/firefox/user.js" "$d/user.js"
 done
+
+mkdir -p ~/Apps
+
+install_repo() {
+  dir_name=$(basename "$1" .git)
+  printf '%s... ' "$dir_name"
+  if [ -d "$HOME/Apps/$dir_name" ]; then
+    (cd "$HOME/Apps/$dir_name"; git pull --ff-only)
+  else
+    (cd ~/Apps; git clone "$1")
+  fi
+}
+
+install_repo https://github.com/junegunn/fzf.git
+install_repo https://github.com/sindresorhus/pure.git
